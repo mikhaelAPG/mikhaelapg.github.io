391994d2966841f42d88dfee20af64ecf617e738
Mikhael <mikhaelapg00@gmail.com>
2021-01-04 23:41:37 +0700

    initial commit

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $databaseHost = 'localhost';
-    $databaseName = 'portfolio';
-    $databaseUsername = 'root';
-    $databasePassword = '';
-
-    $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-?>
